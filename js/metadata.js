f000d3ece9839e4bd930646fa090817b25fb1948
JuanjoML1 <juanjose.moreno.labella@gmail.com>
2026-07-25 23:50:07 +0200

    botón youtube

--- js/metadata.js
+++ js/metadata.js
@@ -21,6 +21,28 @@ function cargarMetadata() {
 
     });
 
+    // Enlaces
+    document.querySelectorAll("[data-md-href]").forEach(el => {
+
+        const ruta = el.dataset.mdHref.split(".");
+
+        let valor = metadata;
+
+        for (const clave of ruta) {
+
+            if (valor == null) break;
+
+            valor = valor[clave];
+        }
+
+        if (valor) {
+            el.href = valor;
+        } else {
+            el.style.display = "none";
+        }
+
+    });
+
 }
 
 document.addEventListener("DOMContentLoaded", cargarMetadata);
